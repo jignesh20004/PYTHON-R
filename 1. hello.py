f415a74300de4e51e8ce7b95e2d5aa5442a8d02e
jignesh20004 <jignesh20004@gmail.com>
2025-02-02 23:35:21 -0500

    change hello

--- 1. hello.py
+++ 1. hello.py
@@ -1,23 +1,23 @@
 
-print("jignesh")
-print("22") 
-A =5
-B =2
+# print("jignesh")
+# print("22") 
+# A =5
+# B =2
 
-print (A + B)
-print (A - B)
-print (A * B)
-print (A / B)
-print (A ** B)
-print (A // B)
-a = 5
-b = 5
-print(f"memory addresses of a:", {id(a)})
-print(f"memory addesses of b:", {id(b)})
-a = input("enter a number")
-b = float(a)
-print(b)
-print(type(b))
+# print (A + B)
+# print (A - B)
+# print (A * B)
+# print (A / B)
+# print (A ** B)
+# print (A // B)
+# a = 5
+# b = 5
+# print(f"memory addresses of a:", {id(a)})
+# print(f"memory addesses of b:", {id(b)})
+# a = input("enter a number")
+# b = float(a)
+# print(b)
+# print(type(b))
 a = 10
 b = 5
 a = True
