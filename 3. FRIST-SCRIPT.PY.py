1fa2a7dcd50096a5f371598324b99bc18ec4edd5
jignesh20004 <jignesh20004@gmail.com>
2025-02-02 23:08:07 -0500

    frist python

---
+++ 3. FRIST-SCRIPT.PY.py
@@ -0,0 +1,9 @@
+ #print(" NAME:- JIGNESH\n", "AGE:- 21\n", "SCHOOL:- SAMITHI")
+ #NAME:- JIGNESH
+ #AGE:- 21
+ #SCHOOL:- SAMITHI
+#print(" NAME:- JIGNESH\n", "AGE:- 21\n", "SCHOOL:- SAMITHI")
+#integer
+int_var = 10
+print("value: ", int_var  )
+print("type:", type(int_var)  )
